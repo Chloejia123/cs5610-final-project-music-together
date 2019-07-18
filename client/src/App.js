2a68124466da8058c9f4dd6e7b4eeb99ed67d903
ruijieshi <shi.ruij@husky.neu.edu>
2019-07-18 09:44:45 +0800

    finish register and alert

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './component/layout/NavBar';
+import Landing from './component/layout/Landing';
+import Register from './component/auth/Register';
+import Login from './component/auth/Login';
+import {Provider} from 'react-redux';
+import store from './store';
+import './App.css';
+import Alert from './component/layout/Alert';
+
+const App = () => (
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <NavBar />
+                <Route exact path='/' component={Landing} />
+                <section className='container'>
+                    <Alert/>
+                    <Switch>
+                        <Route exact path='/register' component={Register} />
+                        <Route exact path='/login' component={Login} />
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
+);
+
+export default App;
