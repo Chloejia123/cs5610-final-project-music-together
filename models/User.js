afd038df026149ce3e1af68831d9ac9f5a628123
Yi Jia <chloe.yijia@gmail.com>
2019-08-09 16:19:21 -0700

    Added role type to user model to include admin role

--- models/User.js
+++ models/User.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    roleType: {
+        type: String,
+        enum: ["GENERAL_USER", "ADMIN"],
+        required: true
+    },
     name: {
         type: String,
         required: true
