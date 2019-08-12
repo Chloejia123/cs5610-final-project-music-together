704122abe6cd94230f1a5944588dc4a1531d4859
Yao Xiao <yaox@Yaos-MacBook-Pro.local>
2019-08-11 21:35:08 -0400

    alphebaticize

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import NavBar from './component/layout/NavBar';
-import Landing from './component/layout/Landing';
-import Register from './component/auth/Register';
+import {Provider} from 'react-redux';
+import { loadUser } from './actions/auth';
+import './App.css';
 import Login from './component/auth/Login';
+import Register from './component/auth/Register';
+import Landing from './component/layout/Landing';
+import Alert from './component/layout/Alert';
+import NavBar from './component/layout/NavBar';
+import Post from './component/post/Post';
+import Posts from './component/posts/posts';
+import PrivateRoute from './component/rounting/PrivateRoute';
+import Profile from './component/profile/Profile';
+import ProfileAll from './component/profile/ProfileAll';
+import ProfileOther from './component/profile/ProfileOther';
 import Search from './component/search/Search';
 import SearchDetail from './component/search/SearchDetail';
-import {Provider} from 'react-redux';
 import store from './store';
-import './App.css';
-import Alert from './component/layout/Alert';
-import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-import Profile from './component/profile/Profile';
-import ProfileOther from './component/profile/ProfileOther';
-import ProfileAll from './component/profile/ProfileAll';
-import Posts from './component/posts/posts'
-import PrivateRoute from './component/rounting/PrivateRoute'
-import Posts from './component/posts/posts'
-import Post from './component/post/Post'
-import PrivateRoute from './component/rounting/PrivateRoute'
 
 
 if (localStorage.token) {
@@ -41,19 +39,19 @@ const App = () => {
                 <section className='container'>
                     <Alert/>
                     <Switch>
-                        <Route exact path='/search' component={Search} />
-                        <Route exact path='/search/details/artists/:id' component={SearchDetail} />
-                        <Route exact path='/search/details/songs/:id' component={SearchDetail} />
-                        <Route exact path='/register' component={Register}/>
                         <Route exact path='/login' component={Login}/>
+                        <PrivateRoute exact path='/posts' component={Posts}/>
+                        <PrivateRoute exact path='/posts/:id' component={Post}/>
                         <Route exact path='/profile' 
                             render={(props) => store.getState().auth.isAuthenticated
                                         ? <Profile />
                                         : <Redirect to='/login' />} />
-                        <Route exact path='/profile/:userId' component={ProfileOther} />
                         <Route exact path='/profiles' component={ProfileAll} />
-                        <PrivateRoute exact path='/posts' component={Posts}/>
-                        <PrivateRoute exact path='/posts/:id' component={Post}/>
+                        <Route exact path='/profile/:userId' component={ProfileOther} />
+                        <Route exact path='/search' component={Search} />
+                        <Route exact path='/search/details/artists/:id' component={SearchDetail} />
+                        <Route exact path='/search/details/songs/:id' component={SearchDetail} />
+                        <Route exact path='/register' component={Register}/>
                     </Switch>
                 </section>
             </Fragment>
